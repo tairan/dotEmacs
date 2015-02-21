efdf169f5537bff00acdb448c8e6b29739311dca
Tairan Wang <tairan.wang@gmail.com>
2015-02-21 19:31:36 +0800

    remove packages for every platform

--- emacs.d/elpa/coffee-mode-0.4.1.1/coffee-mode-autoloads.el
+++
@@ -1,32 +0,0 @@
-;;; coffee-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "coffee-mode" "../../../../.emacs.d/elpa/coffee-mode-0.4.1.1/coffee-mode.el"
-;;;;;;  "b1c98306dc2959c543a9ff59095af214")
-;;; Generated autoloads from ../../../../.emacs.d/elpa/coffee-mode-0.4.1.1/coffee-mode.el
-
-(autoload 'coffee-mode "coffee-mode" "\
-Major mode for editing CoffeeScript.
-
-\(fn)" t nil)
-
-(add-to-list 'auto-mode-alist '("\\.coffee\\'" . coffee-mode))
-
-(add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
-
-;;;***
-
-;;;### (autoloads nil nil ("../../../../.emacs.d/elpa/coffee-mode-0.4.1.1/coffee-mode-autoloads.el"
-;;;;;;  "../../../../.emacs.d/elpa/coffee-mode-0.4.1.1/coffee-mode.el")
-;;;;;;  (21736 14100 864028 213000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; coffee-mode-autoloads.el ends here
--- emacs.d/elpa/markdown-mode-2.0/markdown-mode-autoloads.el
+++
@@ -1,33 +0,0 @@
-;;; markdown-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "markdown-mode" "../../../../.emacs.d/elpa/markdown-mode-2.0/markdown-mode.el"
-;;;;;;  "4055982d87ae4f0073058a6baa5d886f")
-;;; Generated autoloads from ../../../../.emacs.d/elpa/markdown-mode-2.0/markdown-mode.el
-
-(autoload 'markdown-mode "markdown-mode" "\
-Major mode for editing Markdown files.
-
-\(fn)" t nil)
-
-(autoload 'gfm-mode "markdown-mode" "\
-Major mode for editing GitHub Flavored Markdown files.
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("../../../../.emacs.d/elpa/markdown-mode-2.0/markdown-mode-autoloads.el"
-;;;;;;  "../../../../.emacs.d/elpa/markdown-mode-2.0/markdown-mode.el")
-;;;;;;  (21736 14117 501586 815000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; markdown-mode-autoloads.el ends here
